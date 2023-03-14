9ecc6b046faf99ee6f61aac0522f7f47b32367de
udonggi <dongki1882@naver.com>
2023-03-14 16:31:07 +0900

    bracket

---
+++ src/main/java/com/ll/Calc.java
@@ -0,0 +1,62 @@
+package com.ll;
+
+
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
+public class Calc {
+    public static int run(String exp) {
+        exp = stripOuterBrackets(exp);
+        // 단일항이 입력되면 바로 리턴
+        if ( !exp.contains(" ") ) return Integer.parseInt(exp);
+
+        boolean needToMulti = exp.contains(" * ");
+        boolean needToPlus = exp.contains(" + ") || exp.contains(" - ");
+
+        boolean needToCompound = needToMulti && needToPlus;
+
+        if ( needToCompound ) {
+            String[] bits = exp.split(" \\+ ");
+
+            String newExp = Arrays.stream(bits)
+                    .mapToInt(Calc::run)
+                    .mapToObj(e -> e + "")
+                    .collect(Collectors.joining(" + "));
+
+            return run(newExp);
+        }
+        else if ( needToPlus ) {
+            exp = exp.replaceAll("- ", "+ -");
+
+            String[] bits = exp.split(" \\+ ");
+
+            int sum = 0;
+
+            for (int i = 0; i < bits.length; i++) {
+                sum += Integer.parseInt(bits[i]);
+            }
+
+            return sum;
+        }
+        else if ( needToMulti ) {
+            String[] bits = exp.split(" \\* ");
+
+            int sum = 1;
+
+            for (int i = 0; i < bits.length; i++) {
+                sum *= Integer.parseInt(bits[i]);
+            }
+
+            return sum;
+        }
+
+        throw new RuntimeException("올바른 계산식이 아닙니다.");
+    }
+
+    private static String stripOuterBrackets(String exp) {
+        if(exp.charAt(0) == '(' && exp.charAt(exp.length() -1) == ')'){
+            exp = exp.substring(1,exp.length()-1);
+        }
+        return exp;
+    }
+}
--- src/main/java/com/ll/Main.java
+++ src/main/java/com/ll/Main.java
@@ -1,7 +1,41 @@
 package com.ll;
 
+import java.util.Arrays;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args){
+
+        System.out.println(Calc.run("10 + 5 * 2"));
+
     }
 }
+
+//class Polynomial {
+//    String numbers;
+//    Polynomial(String numbers){
+//        this.numbers = numbers;
+//    }
+//
+//    public int calc(){
+//        int result = 0;
+//        numbers = numbers.replaceAll(" ", "");
+//
+//        for(int i = 0 ; i < numbers.length(); i ++){
+//            if(Character.isDigit(numbers.charAt(i))){
+//                if(Character.isDigit(numbers.charAt(i+1))){
+//                    int a = numbers.charAt(i) - '0';
+//                    int b = numbers.charAt(i+1) - '0';
+//                    result = 10 * a + b;
+//                }
+//                else{
+//                    result = numbers.charAt(i) - '0';
+//                }
+//            }
+//            else{
+//                result += numbers.charAt(i+1) - '0';
+//                break;
+//            }
+//        }
+//        return result;
+//    }
+//}
